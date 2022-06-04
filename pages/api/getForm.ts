75aa8ab4467617d3185af922d34ab4e9b648690a
Matthew Ngan <matthewngan3@gmail.com>
2022-06-04 15:32:42 +0800

    .

--- pages/api/getForm.ts
+++ pages/api/getForm.ts
@@ -2,8 +2,7 @@ export default async function handler(req, res) {
   if (req.method === 'GET') {
     const data = await fetch(`http://${process.env.GOOGLE_FORM_API_SERVER}/getform?form_id=${req.query.id}`, {method: 'GET', headers: {
       'Authorization': req.headers.authorization
-    }}).then(response => {console.log(response); return response.json()});
-    console.log(data);
+    }}).then(response => response.json());
     res.status(200).json(data);
   }
 }
